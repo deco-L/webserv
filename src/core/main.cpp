0aeed87c160e4e9ddb9d7f60a8aa750731b43a29
deco-L <scotchwitchbroom@gmail.com>
2024-11-01 23:58:15 +0900

    ✨ add multiple server

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
-/*   Updated: 2024/11/01 23:43:13 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/11/01 23:51:18 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,6 @@ int main(int argc, char **argv, char **envp)
   (void)envp;
 
   configMain(root.config, argc, argv);
-  const std::vector<ConfigServer>& config = root.config.getServers();
-  for (std::vector<ConfigServer>::const_iterator it = config.begin(); it != config.end(); it++) {
-    for (std::vector<std::pair<std::string, std::string> >::const_iterator listen_it = it->listen.begin(); listen_it != it->listen.end(); listen_it++) {
-        std::cout << "IP: " << listen_it->first << ", Port: " << listen_it->second << std::endl;
-    }
-  }
   socketMain(root.socket, root.config.getServers());
   eventLoop(root.socket, root.config.getServers());
   socketEnd(root.socket);
